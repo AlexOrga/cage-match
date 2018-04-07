aa3979450012e8175dfff51fba3e2e593e4358bb
Alex Orga <alexorga.dev@gmail.com>
2018-04-06 20:37:41 -0500

    wrote generic xhr request

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,3 +1,11 @@
 const printToDom = (divId, string) => {
     document.getElementById(divId).innerHTML = string;
 };
+
+const genericXhrRequest = (player, callback) => {
+    let myRequest = new XMLHttpRequest();
+    myRequest.addEventListener('load', callback);
+    myRequest.addEventListener('error', xhrError);
+    myRequest.open('GET', `https://teamtreehouse.com/${player}.json`);
+    myRequest.send();
+};
