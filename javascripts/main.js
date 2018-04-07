aa042a9dec21c7e222d2b0838c701040016e7a4d
Alex Orga <alexorga.dev@gmail.com>
2018-04-06 20:41:57 -0500

    added and error function

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,6 +2,10 @@ const printToDom = (divId, string) => {
     document.getElementById(divId).innerHTML = string;
 };
 
+function xhrError() {
+    alert('Sorry, something went wrong. Please contact the coder and tell him to get his act together');
+}
+
 const genericXhrRequest = (player, callback) => {
     let myRequest = new XMLHttpRequest();
     myRequest.addEventListener('load', callback);
